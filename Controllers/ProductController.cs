dfa0edbfe076bd2d82fc68dfd02d89a8cb470ed3
vtorres96 <victorcdc96@gmail.com>
2021-01-10 15:58:02 -0300

    added update nad delete methods in Product Controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -16,28 +16,49 @@ namespace dotnet_ef_api.Controllers
     [HttpGet]
     public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
     {
+			try
+			{
         var products = await context.Products.Include(x => x.Category).ToListAsync();
         return products;
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
     {
+      try 
+      {
         var product = await context.Products.Include(x => x.Category)
           .AsNoTracking()
           .FirstOrDefaultAsync(x => x.Id == id);
         return product;
+      }
+			catch (Exception ex)
+			{
+					return BadRequest($"Error: {ex.Message}");
+			}
     }
 
     [HttpGet]
     [Route("categories/{id}")]
     public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
     {
+			try
+			{
         var products = await context.Products.Include(x => x.Category)
           .AsNoTracking()
           .Where(x => x.Category.Id == id)
           .ToListAsync();
         return products;
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
     }
 
     [HttpPost]
@@ -45,24 +66,62 @@ namespace dotnet_ef_api.Controllers
 				[FromServices] DataContext context, 
 				[FromBody] Product model)
 		{
+			try
+			{
 				if(ModelState.IsValid)
 				{
-						context.Products.Add(model);
-						await context.SaveChangesAsync();
-						return model;
+					context.Products.Add(model);
+					await context.SaveChangesAsync();
+					return model;
 				} else {
-						return BadRequest(ModelState);
+					return BadRequest(ModelState);
 				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
 		}
 
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
+		public async Task<IActionResult> Put(
+				int id, 
+				[FromServices] DataContext context, 
+				[FromBody] Product model)
+		{
+			try
+			{
+				var product = await context.Products.AsNoTracking()
+					.Where(product => product.Id == id)
+					.FirstOrDefaultAsync();
+				if(product == null) return NotFound("Product not found");
+
+				context.Products.Update(model);
+				await context.SaveChangesAsync();
+				return Ok(model);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
+		}
 
     [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
+		public async Task<IActionResult> Delete(int id, [FromServices] DataContext context)
+		{
+			try
+			{
+				var product = await context.Products.FirstOrDefaultAsync(product => product.Id == id);
+				if(product == null) return NotFound("Product not found");
+
+				context.Remove(product);
+				await context.SaveChangesAsync();
+				return Ok(new { message = "Product deleted" });
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
+		}
   }
 }
