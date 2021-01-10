5b5996d044f42b77343b84bdc16d531c1aaf16bb
vtorres96 <victorcdc96@gmail.com>
2021-01-10 15:58:11 -0300

    added update nad delete methods in Category Controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,14 +16,31 @@ namespace dotnet_ef_api.Controllers
 		[HttpGet]
 		public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
 		{
+			try
+			{
 				var categories = await context.Categories.ToListAsync();
-				return categories;
+				return Ok(categories);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
 		}
 
 		[HttpGet("{id}")]
-		public ActionResult<string> Get(int id)
+		public async Task<ActionResult<Category>> GetById([FromServices] DataContext context, int id)
 		{
-				return "value";
+			try
+			{
+				var category = await context.Categories
+					.AsNoTracking()
+					.FirstOrDefaultAsync(category => category.Id == id);
+        return Ok(category);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
 		}
 
 		[HttpPost]
@@ -31,24 +48,62 @@ namespace dotnet_ef_api.Controllers
 				[FromServices] DataContext context, 
 				[FromBody] Category model)
 		{
+			try
+			{
 				if(ModelState.IsValid)
 				{
-						context.Categories.Add(model);
-						await context.SaveChangesAsync();
-						return model;
+					context.Categories.Add(model);
+					await context.SaveChangesAsync();
+					return model;
 				} else {
-						return BadRequest(ModelState);
+					return BadRequest(ModelState);
 				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
 		}
 
 		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
+		public async Task<IActionResult> Put(
+				int id, 
+				[FromServices] DataContext context, 
+				[FromBody] Category model)
 		{
+				try
+				{
+					var category = await context.Categories.AsNoTracking()
+						.Where(category => category.Id == id)
+						.FirstOrDefaultAsync();
+					if(category == null) return NotFound("Category not found");
+
+					context.Categories.Update(model);
+					await context.SaveChangesAsync();
+					return Ok(model);
+				}
+				catch (Exception ex)
+				{
+					return BadRequest($"Error: {ex.Message}");
+				}
 		}
 
 		[HttpDelete("{id}")]
-		public void Delete(int id)
+		public async Task<IActionResult> Delete(int id, [FromServices] DataContext context)
 		{
+			try
+			{
+				var category = await context.Categories.FirstOrDefaultAsync(category => category.Id == id);
+				if(category == null) return NotFound("Category not found");
+
+				context.Remove(category);
+				await context.SaveChangesAsync();
+				return Ok(new { message = "Category deleted" });
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Error: {ex.Message}");
+			}
 		}
 	}
 }
